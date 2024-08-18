9ae17430f2a467f671821953bbfeec328bb4a8c8
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-08-18 14:32:51 +0530

    chore: Update About page layout and content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,51 @@
+import Button24 from "../components/Button24";
+
 export default function About() {
   return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <h1 className=" font-anton text-white text-6xl tracking-widest">About</h1>
+    <div className="bg-black w-full flex flex-col justify-center items-center px-20 h-[100vh]">
+      <h1 className=" font-anton text-white text-6xl tracking-widest uppercase">
+        About
+      </h1>
+      <div className="flex gap-10 text-white justify-between font-roboto-mono w-full">
+        <div className="flex flex-col gap-2 justify-center items-center w-full ">
+          <h1 className="uppercase text-4xl font-bold">Pre-Event</h1>
+          <div className="h-full w-full bg-about rounded-2xl p-4">
+            Getting into a loop of ideas, but finding out where to showcase
+            them? It&apos;s time to think big and act fast!💡 Start building
+            projects and join us at DUHacks 3.0! 🛠️ DUHacks 3.0 is a 36-hour
+            national-level hackathon organized by the Google Developer Student
+            Club, DDU🌍 This event provides a platform for passionate
+            developers, designers, and enthusiasts to come together and
+            transform their ideas into reality. It&apos;s not just an
+            opportunity to showcase technical skills; it&apos;s a chance to
+            network, learn, and have a memorable time with like-minded
+            individuals.🤝 Your keyboard is your sword, your idea is your
+            challenge, code is your vision, and DUHacks 3.0 is your stage! 🚨
+            See you all in the hackathon!🔍💻
+          </div>
+          <Button24 text={"Pre-Event"} />
+        </div>
+        <div
+          className="flex flex-col gap-2 justify-center items-center w-full "
+        >
+          <h1 className="uppercase text-4xl font-bold">24Hr Challenge</h1>
+          <div className="h-full w-full bg-about rounded-2xl p-4">
+            Getting into a loop of ideas, but finding out where to showcase
+            them? It&apos;s time to think big and act fast!💡 Start building
+            projects and join us at DUHacks 3.0! 🛠️ DUHacks 3.0 is a 36-hour
+            national-level hackathon organized by the Google Developer Student
+            Club, DDU🌍 This event provides a platform for passionate
+            developers, designers, and enthusiasts to come together and
+            transform their ideas into reality. It&apos;s not just an
+            opportunity to showcase technical skills; it&apos;s a chance to
+            network, learn, and have a memorable time with like-minded
+            individuals.🤝 Your keyboard is your sword, your idea is your
+            challenge, code is your vision, and DUHacks 3.0 is your stage! 🚨
+            See you all in the hackathon!🔍💻
+          </div>
+          <Button24 text={"24Hr Challenge"} />
+        </div>
+      </div>
     </div>
   );
 }
