e6474cd69b96dfd4b8d8109971034c2ac93bf152
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-08-18 15:22:46 +0530

    ✨ feat: Update Clubs page to display club cards with information

--- src/pages/Clubs.jsx
+++ src/pages/Clubs.jsx
@@ -1,3 +1,25 @@
+// import React from "react";
+import { clubs } from "../../constants";
+import ClubCards from "../components/ClubCards";
 export default function Clubs() {
-  return <div></div>;
+  return (
+    <div className="text-white h-full w-full flex flex-col justify-center items-center gap-24">
+      <h1 className="text-6xl font-anton tracking-widest uppercase ">
+        Organising Clubs
+      </h1>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="h-full w-full flex gap-4 flex-wrap justify-center items-center">
+          {clubs.map((club, index) => {
+            return (
+              <ClubCards
+                key={index}
+                name={club.name}
+                description={club.description}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
