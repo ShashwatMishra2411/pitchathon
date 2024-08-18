3760c8faf95bc18bbf4f80d64ab10d01983e5f94
ShubhiCodess <shubhicodes37@gmail.com>
2024-08-18 21:43:34 +0530

    lesgo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,12 +2,12 @@ import Button24 from "../components/Button24";
 
 export default function About() {
   return (
-    <div className="km w-[90vw] flex flex-col justify-center items-center h-full rounded-xl px-9 py-10 bg-opacity-10">
+    <div className="des w-[90vw] flex flex-col justify-center items-center h-full rounded-xl px-9 py-10 ">
       <h1 className=" font-anton text-white text-6xl tracking-widest uppercase">
         About
       </h1>
       <div className="flex gap-10 text-white justify-between font-roboto-mono w-full">
-        <div className="flex flex-col gap-2 justify-center items-center w-full ">
+        <div className="flex flex-col gap-4 justify-center items-center w-full ">
           <h1 className="uppercase text-xl xl:text-4xl font-bold">Pre-Event</h1>
           <div className="h-full w-full bg-about rounded-2xl p-4">
             Getting into a loop of ideas, but finding out where to showcase
@@ -23,7 +23,10 @@ export default function About() {
             challenge, code is your vision, and DUHacks 3.0 is your stage! 🚨
             See you all in the hackathon!🔍💻
           </div>
-          <Button24 text={"Pre-Event"} />
+          <button class="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+  Pre Event
+</button>
+
         </div>
         <div className="flex flex-col gap-2 justify-center items-center w-full ">
           <h1 className="uppercase text-xl xl:text-4xl text-nowrap font-bold">
@@ -43,7 +46,10 @@ export default function About() {
             challenge, code is your vision, and DUHacks 3.0 is your stage! 🚨
             See you all in the hackathon!🔍💻
           </div>
-          <Button24 text={"24Hr Challenge"} />
+          <button class="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4  origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+  24 Hr Challenge
+</button>
+
         </div>
         {/* <div className="flex flex-col gap-2 justify-center items-center w-full ">
           <h1 className="uppercase text-xl xl:text-4xl text-center text-nowrap font-bold">
