77ce05e507ac77ad82157e897ca3e7cc8971e761
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-08-24 19:04:30 +0530

    refactor: Update Coordinators page layout and styling

--- src/pages/Coordinators.jsx
+++ src/pages/Coordinators.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { faculties } from "../../constants";
 import Card from "../components/Card";
 
@@ -14,14 +13,13 @@ export default function Coordinators() {
         <h1 className="text-4xl uppercase">Faculty Coordinators</h1>
         <div
           // Tailwind's responsive grid classes
-          className="h-full w-full overflow-hidden grid gap-10 justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+          className="h-full w-full overflow-hidden grid gap-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 justify-items-center"
         >
           {faculties.map((faculty, index) => {
             return <Card faculty={faculty} key={index} />;
           })}
         </div>
       </div>
-      
     </div>
   );
 }
