b1ca9ce7a95838c1e33895464e317b572960f6fc
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-09-02 21:23:18 +0530

    chore: Update About page layout and styling for responsiveness

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div
       id="about"
-      className="des w-[90vw] flex flex-col justify-center items-center h-full rounded-xl px-4 lg:px-9 py-10 "
+      className="des w-[90vw] flex flex-col justify-center items-center h-full rounded-xl px-2 lg:px-9 py-10 "
     >
       <h1 className="font-anton text-white text-6xl tracking-widest uppercase">
         About
@@ -16,9 +16,9 @@ export default function About() {
             are designed to engage participants and spark creativity across
             various domains. Whether you&apos;re passionate about arts, eager to
             tackle a hackathon, or ready to dive into some competitive gaming,
-            there&rsquo;s something for everyone. These pre-events set the stage for
-            the main challenge, offering a chance to connect, collaborate, and
-            warm up your innovative spirit. Get involved and be part of the
+            there&rsquo;s something for everyone. These pre-events set the stage
+            for the main challenge, offering a chance to connect, collaborate,
+            and warm up your innovative spirit. Get involved and be part of the
             journey leading to the grand finale!
           </div>
           <button className="group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 underline underline-offset-2 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur hover:underline hover:underline-offset-4 origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 h-16 w-64 border text-left p-3 text-gray-50 text-base font-bold rounded-lg overflow-hidden before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg after:absolute after:z-10 after:w-20 after:h-20 after:content[''] after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
